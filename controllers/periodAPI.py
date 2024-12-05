aef6f610a20235e14f52fcfe287c2bd6a0f0a3a6
Tran Thanh Tung <102210138@sv1.dut.udn.vn>
2024-12-06 01:01:09 +0700

    feat: added post for period at POST /periods

--- controllers/periodAPI.py
+++ controllers/periodAPI.py
@@ -2,9 +2,12 @@ from flask import Blueprint, request, jsonify
 from models import db
 from models.schedule import Schedule
 from models.period import Period
+from datetime import datetime
+
 
 period_bp = Blueprint('period_bp', __name__)
 schedule_bp = Blueprint('schedule_bp', __name__)
+
 
 @period_bp.route('/periods', methods=['GET'])
 def get_all_periods():
@@ -21,6 +24,7 @@ def get_all_periods():
 		print(e)
 		return jsonify({'error': 'An error occurred while retrieving periods.'}), 500
 
+
 @period_bp.route('/periods/<int:id>', methods=['GET'])
 def get_period(id):
 	try:
@@ -36,3 +40,32 @@ def get_period(id):
 	except Exception as e:
 		print(e)
 		return jsonify({'error': 'An error occurred while retrieving period.'}), 500
+
+
+@period_bp.route('/periods', methods=['POST'])
+def create_period():
+    try:
+        data = request.get_json()
+        with db.session.begin():
+            # Create a new period
+            start_date = datetime.strptime(
+                data['start_date'], '%Y-%m-%d').date()
+            end_date = datetime.strptime(
+                data['end_date'], '%Y-%m-%d').date() if data.get('end_date') else None
+            period = Period(
+                drug_name=data['drug_name'], start_date=start_date, end_date=end_date)
+            db.session.add(period)
+            db.session.flush()  # Ensure period.id is available
+
+            schedules = data['schedules']
+            for schedule in schedules:
+                # Create a new schedule
+                time = datetime.strptime(schedule['time'], '%H:%M').time()
+                s = Schedule(period_id=period.id, message=schedule['message'], time=time)
+                db.session.add(s)
+
+        return jsonify({'message': 'Period created successfully.'}), 201
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+        return jsonify({'error': str(e)}), 400
