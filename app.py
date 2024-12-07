24935e69c68f7ee4a61c7896485d73b2d9ac0c20
Tran Thanh Tung <102210138@sv1.dut.udn.vn>
2024-12-07 23:45:05 +0700

    fix: add cors

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import db
 from controllers.periodAPI import period_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///schedules.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+CORS(app)
 
 db.init_app(app)
 migrate = Migrate(app, db)
